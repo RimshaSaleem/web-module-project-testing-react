ea0b3650b613adc9e94e6109fb5bf1e5745d687a
RimshaSaleem <rimshatif8@gmail.com>
2021-08-16 22:48:24 -0700

    wip

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -14,14 +14,22 @@ const testEpisode = {
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name:"",
+    image:null,
+    season:1,
+    number:1,
+    summary:'Its a summary',
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode ={testEpisode} />)
 });
 
+
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode ={testEpisode} />)
 });
 
 test("renders default image when image is not defined", ()=>{
